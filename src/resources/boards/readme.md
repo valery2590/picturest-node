16d12292ca7b4adc054b10f6439820ee7e2abded
Eudald <eaarranz@gmail.com>
2020-11-29 17:50:20 +0100

    first commit

---
+++ src/resources/boards/readme.md
@@ -0,0 +1,1 @@
+Todo lo relativo a boards
---
+++ src/server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const { json, urlencoded } = require('body-parser');
+const morgan = require('morgan');
+const config = require('./config.js');
+const cors = require('cors');
+// const userRouter = require('./resources/user/user.router');
+
+const app = express();
+
+app.disable('x-powered-by');
+
+app.use(cors());
+app.use(json());
+app.use(urlencoded({ extended: true }));
+app.use(morgan('dev'));
+
+// app.use('/api/user', userRouter);
+
+const start = async () => {
+  try {
+    app.listen(config.port, () => {
+      console.log(`REST API on http://localhost:${config.port}/api`);
+    });
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+module.exports = {
+  start,
+  app,
+};
