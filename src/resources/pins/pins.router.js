c7e868027db2f529446dedfdbe334fab6e7af068
Eudald <eaarranz@gmail.com>
2020-11-29 19:45:59 +0100

    feat: open exercise

--- src/resources/pins/pins.router.js
+++ src/resources/pins/pins.router.js
@@ -1,13 +1,1 @@
-const { Router } = require('express');
-const pinsController = require('./pins.controller');
-const router = Router();
-
-router.route('/').get(pinsController.getAll).post(pinsController.create);
-
-router
-  .route('/:id')
-  .get(pinsController.getOne)
-  .put(pinsController.update)
-  .delete(pinsController.remove);
-
-module.exports = router;
+// TODO
